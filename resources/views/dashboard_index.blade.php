46112b9dfe7933189e6a673cbabf85eea1daa40f
Nabilla-005 <nabilla.hasya@mhs.itenas.ac.id>
2024-12-17 18:15:00 +0700

    dashboard admin

---
+++ resources/views/dashboard_index.blade.php
@@ -0,0 +1,9 @@
+@extends('mylayout')
+@section('content')
+    <div class="card">
+        <div class="card-body">
+            <h5 class="card-title>Dashboard</h5>
+            Dashboard
+        </div>
+    </div>
+@endsection
